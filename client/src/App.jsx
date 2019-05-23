9518b402ad3afca4825246015a3632b09cabb885
Mike <diazdeleonjhon@gmail.com>
2019-05-23 11:16:16 -0500

    Routing between pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import { BrowserRouter as Route, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
 import NavBar from './components/NavBar.jsx';
 import HomePage from './components/HomePage.jsx';
-import Login from './components/Login.jsx';
-import Post from './components/Post.jsx';
-import Register from './components/Register.jsx';
+// import Login from './components/Login.jsx';
+// import Post from './components/Post.jsx';
+// import Register from './components/Register.jsx';
 
 
 
@@ -15,9 +15,15 @@ class App extends Component {
     return (
       <div>
         <NavBar/>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/homePage' component={HomePage} />
+            <Route path='/login' component={Login} />
+          </Switch>
         <HomePage/>
       </div>
     );
   }
 }
 export default hot(module)(App);
+
