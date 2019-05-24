11963c7ca821f2208176e0925f063a47294af65e
Mike <diazdeleonjhon@gmail.com>
2019-05-24 11:21:12 -0500

    carousel working

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import AliceCarousel from 'react-alice-carousel';
-
+import Carousel from 'nuka-carousel';
 
 class HomePage extends Component {
-    //renders data to the DOM
+
   render() {
-    const handleOnDragStart = e => e.preventDefault()
     return (
-      <AliceCarousel mouseDragEnabled >
-        <img src="https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000214591_sd.jpg;maxHeight=200;maxWidth=200" onDragStart={handleOnDragStart} className="yours-custom-class" />
-        <img src="https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000214591_sd.jpg;maxHeight=200;maxWidth=200" onDragStart={handleOnDragStart} className="yours-custom-class" />
-        <img src="https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000214591_sd.jpg;maxHeight=200;maxWidth=200" onDragStart={handleOnDragStart} className="yours-custom-class" />
-        <img src="https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000214591_sd.jpg;maxHeight=200;maxWidth=200" onDragStart={handleOnDragStart} className="yours-custom-class" />
-        <img src="https://pisces.bbystatic.com/image2/BestBuy_MX/images/products/1000/1000214591_sd.jpg;maxHeight=200;maxWidth=200" onDragStart={handleOnDragStart} className="yours-custom-class" />
-      </AliceCarousel>
-    )
+      <div style={{ width: "60%", margin: "auto"}}>
+        <Carousel >
+          <img src="https://i.imgur.com/x9kgxN3.jpg" />
+          <img src="https://i.imgur.com/x9kgxN3.jpg" />
+          <img src="https://i.imgur.com/x9kgxN3.jpg" />
+          <img src="https://i.imgur.com/x9kgxN3.jpg" />
+          <img src="https://i.imgur.com/x9kgxN3.jpg" />
+        </Carousel>
+      </div>      
+    );
   }
-}
+};
 
 export default HomePage;
