3398d7f5b9f39b76598354c6adddfe29e8c896ad
Mike <diazdeleonjhon@gmail.com>
2019-05-24 11:49:45 -0500

    Implenting more carousl with their appropriate pic

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -5,15 +5,63 @@ class HomePage extends Component {
 
   render() {
     return (
-      <div style={{ width: "60%", margin: "auto"}}>
-        <Carousel >
-          <img src="https://i.imgur.com/x9kgxN3.jpg" />
-          <img src="https://i.imgur.com/x9kgxN3.jpg" />
-          <img src="https://i.imgur.com/x9kgxN3.jpg" />
-          <img src="https://i.imgur.com/x9kgxN3.jpg" />
-          <img src="https://i.imgur.com/x9kgxN3.jpg" />
-        </Carousel>
-      </div>      
+      <div>
+        <h2><center>Electronics</center></h2>
+        <div style={{ width: "35%", margin: "auto"}}>
+          <Carousel >
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+            <img src="https://i.imgur.com/prg5Rno.jpg" />
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+            <img src="https://i.imgur.com/prg5Rno.jpg" />
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+          </Carousel>
+        </div>
+
+        <h2><center>Automotive</center></h2>
+        <div style={{ width: "35%", margin: "auto" }}>
+          <Carousel >
+            <img src="https://i.imgur.com/7okyF4q.jpg" />
+            <img src="https://i.imgur.com/L07NVYm.jpg" />
+            <img src="https://i.imgur.com/7okyF4q.jpg" />
+            <img src="https://i.imgur.com/L07NVYm.jpg" />
+            <img src="https://i.imgur.com/7okyF4q.jpg" />
+          </Carousel>
+        </div>
+
+        <h2><center>Jewelery</center></h2>
+        <div style={{ width: "35%", margin: "auto" }}>
+          <Carousel >
+            <img src="https://i.imgur.com/TvjlpIs.jpg" />
+            <img src="https://i.imgur.com/QUK6BGz.jpg" />
+            <img src="https://i.imgur.com/TvjlpIs.jpg" />
+            <img src="https://i.imgur.com/QUK6BGz.jpg" />
+            <img src="https://i.imgur.com/TvjlpIs.jpg" />
+          </Carousel>
+        </div>
+        
+
+        <h2><center>Home & Living</center></h2>
+        <div style={{ width: "35%", margin: "auto" }}>
+          <Carousel >
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+            <img src="https://i.imgur.com/prg5Rno.jpg" />
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+            <img src="https://i.imgur.com/prg5Rno.jpg" />
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+          </Carousel>
+        </div>
+
+        <h2><center>Other</center></h2>
+        <div style={{ width: "35%", margin: "auto" }}>
+          <Carousel >
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+            <img src="https://i.imgur.com/prg5Rno.jpg" />
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+            <img src="https://i.imgur.com/prg5Rno.jpg" />
+            <img src="https://i.imgur.com/x9kgxN3.jpg" />
+          </Carousel>
+        </div>
+      </div>
     );
   }
 };
