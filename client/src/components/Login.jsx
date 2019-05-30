f85e2a24fb664889b73731d5fe1b0e094a6b2272
Mike <diazdeleonjhon@gmail.com>
2019-05-29 20:53:33 -0500

    You can now half way log in with google

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+//later might need need css for login
+//import './App.css'
+import GoogleLogin from 'react-google-login';
+
+
 
 class Login extends Component {
-    //renders data to the DOM
-    render() {
-    return (
-			<h1>I am the login</h1>
-    )
-  };
+  render() {
+  
+  const responseGoogle = (response) => {
+    console.log(response)
+  }
+  return (
+		<div>
+      <GoogleLogin
+        clientId="325715847516-po483aaotpaivdafht54d4ltrv9fd4g3.apps.googleusercontent.com"
+        buttonText="LOGIN WITH GOOGLE"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+    </div>
+  )};
 };
 
 export default Login;
